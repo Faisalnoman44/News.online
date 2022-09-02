e10614d948aa2d585d2afb457d85e48c87808ef2
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-09-03 03:08:41 +0600

    news menu fixed

--- js/app.js
+++ js/app.js
@@ -10,44 +10,46 @@ const showNewsMenu = async (newsAll) => {
         console.log(news)
         const li = document.createElement('li');
         li.innerHTML = `
-        <a onclick="loadNews()">${news.category_name}</a>
+        <a onclick="loadNews(${news.category_id})">${news.category_name}</a>
         `;
-        newsMenu.appendChild(li);
+        newsMenu.appendChild(li)
     })
 }
 
-const loadNews = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a');
+const loadNews = async (id) => {
+    console.log(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/${id}`);
+    console.log(res)
     const data = await res.json();
-    displayNews(data.data[0]);
+    console.log(data);
 
 
 }
 
-const displayNews = async (news) => {
-    console.log(news)
-    const newsCard = document.getElementById('news-card');
-    newsCard.innerHTML = `
-    <div class="card card-side bg-base-100 shadow-xl">
-  <figure><img class="h-full" src="${news.image_url}/100/180/arch" alt="Movie"></figure>
-  <div class="card-body">
-    <h2 class="card-title">New movie is released!</h2>
-    <p>${news.details.slice(0,200)+"..."}</p>
-    <div class="flex gap-10">
-        <div>
-            <p>${news.author.name}</p>
-            <p>${news.author.published_date}</p>
-        </div>
-        <p>Views: ${news.total_view}</p>
-        <div>
-            <p>rating: ${news.rating.number}</p>
-        </div>
-    </div>
-  </div>
-</div>
-    `
+// const displayNews = async (news) => {
+//     console.log(news)
+//     const newsCard = document.getElementById('news-card');
+//     newsCard.innerHTML = `
+//     <div class="card card-side bg-base-100 shadow-xl">
+//   <figure><img class="h-full" src="${news.image_url}/100/180/arch" alt="Movie"></figure>
+//   <div class="card-body">
+//     <h2 class="card-title">New movie is released!</h2>
+//     <p>${news.details}</p>
+//     <div class="flex gap-10">
+//         <div>
+//             <p>${news.author.name}</p>
+//             <p>${news.author.published_date}</p>
+//         </div>
+//         <p>Views: ${news.total_view}</p>
+//         <div>
+//             <p>rating: ${news.rating.number}</p>
+//         </div>
+//     </div>
+//   </div>
+// </div>
+//     `
 
-}
+// }
 
-loadNews();
+// loadNews();
 loadNewsCategory()
